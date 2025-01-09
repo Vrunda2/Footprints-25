993406bae5061e0ef13ea8c2e7bb8de30849d571
Vrunda2 <vrundaradadiya38@gmail.com>
2025-01-09 23:27:28 +0530

    final changes in segment home

--- src/components/HomePage/Home/Home.jsx
+++ src/components/HomePage/Home/Home.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import "./Home.css";
-import mainHeader from "../../../assets/FP25PH.png";
+import mainHeader from "../../../assets/25header.png";
 
 function Home() {
-    return (
-      <>
-        <div className="home-gif">
-          <img
-            src={mainHeader}
-            alt=""
-            id="home-header"
-          />
-        </div>
-      </>
-    );
+  return (
+    <>
+      <div className="home-gif">
+        <img
+          src={mainHeader}
+          alt="Main Header"
+          id="home-header"
+          className="zoom-animation"
+        />
+      </div>
+    </>
+  );
 }
+
 export default Home;

689af4f1dbd19e809c56400fbba0cba4d29df7b8
ayesha1209 <ayeshapatel0713@gmail.com>
2025-01-13 09:16:40 +0530

    animation changes

--- src/components/WebTeam/Webteam.jsx
+++ src/components/WebTeam/Webteam.jsx
@@ -1,251 +1,119 @@
-import React, {useEffect} from 'react'
-import Sponsors from "../Common/SponsorSlide/Sponsors";
-import Footer from "../Common/Footer/Footer";
-import Heading from "../Common/Headings/Heading";
-import "./Webteam.css";
-import WebteamData from "../../Data/Webteam";
-import { useMousePosition } from '../../CustomHooks/useMousePosition';
+import React from 'react';
+import './WebTeam.css';
+// Import all images
+import ayeshaImage from '../../assets/images2/ayesha.jpg';
+import vrundaImage from '../../assets/images2/vrunda.jpg';
+import meghanaImage from '../../assets/images2/meghana.jpg';
+import satchitImage from '../../assets/images2/satchitt.png';
+import shubhImage from '../../assets/images2/shubh2.png';
+import mannImage from '../../assets/images2/mann.jpg';
+import meghImage from '../../assets/images2/megh2.png';
 
-function WebTeam() {
-
-  useEffect(() => {
-  
-    document.title="Website Team | FootPrints'25"
-
-  }, [])
-
-  const position = useMousePosition();
+const WebTeam = () => {
+  const teamMembers = [
+    {
+      name: 'Ayesha Patel',
+      image: ayeshaImage,
+      social: {
+        linkedin: 'http://www.linkedin.com/in/ayeshapatel07',
+        github: 'https://github.com/ayesha1209'
+      }
+    },
+    {
+      name: 'Vrunda Radadiya',
+      image: vrundaImage,
+      social: {
+        linkedin: 'https://www.linkedin.com/in/vrunda-radadiya-8a1a43257/',
+        github: 'https://github.com/Vrunda2'
+      }
+    },
+    {
+      name: 'Meghana Vasava',
+      image: meghanaImage,
+      social: {
+        linkedin: 'https://www.linkedin.com/in/meghana-vasava-17242a261/',
+        github: 'https://github.com/meghanavasava'
+      }
+    },
+    {
+      name: 'Satchit Bhatt',
+      image: satchitImage,
+      social: {
+        linkedin: 'https://www.linkedin.com/in/satchit-bhatt-9933a1253',
+        github: 'https://github.com/Satchit2'
+      }
+    },
+    {
+      name: 'Shubh Doshi',
+      image: shubhImage,
+      social: {
+        linkedin: 'https://www.linkedin.com/in/shubh-doshi-921337256/',
+        github: 'https://github.com/shubhdoshi21'
+      }
+    },
+    {
+      name: 'Mann Chopda',
+      image: mannImage,
+      social: {
+        linkedin: 'https://www.linkedin.com/in/man-chopda-63a012278/',
+        github: 'https://github.com/mann15'
+      }
+    },
+    {
+      name: 'Megh Prajapati',
+      image: meghImage,
+      social: {
+        linkedin: 'https://www.linkedin.com/in/megh-prajapati-ab13a82b3',
+        github: 'https://github.com/Meghhhhh'
+      }
+    }
+  ];
 
   return (
-    <>
-      <div className="webteam_header_img" id="parent_div">
-        <div className="text-on-image-webteam">
-          <Heading id="heading_ourteam" title="WEB TEAM" />
-        </div>
-      </div>
-      <div className="container">
-        <div className="coordinatesDiv">
-          <div className="memcardsss row my-5">
-            {WebteamData.map((element) => {
-              return (
-                // Original
-                // <div
-                //   key={element.id}
-                //   className="memBlock col-sm-6 col-md-5 col-lg-3 profile"
-                // >
-                //   <div className="img-box">
-                //     <img
-                //       src={element.imgSrc}
-                //       alt={element.name}
-                //       className="memImg"
-                //     />
-                //     <ul>
-                //       <li>
-                //         <a href={element.linked} target="_blank">
-                //           <i className="fa-brands fa-linkedin"></i>
-                //         </a>
-                //       </li>
-                //     </ul>
-                    
-                //   </div>
-                //   <p className="memName">{element.name}</p>
-                // </div>
-
-              // First try sahpe
-                // <div className="cardsss cardsss--two ">
-                //   <img
-                //     src={element.imgSrc}
-                //     alt={element.name}
-                //     className="img-responsive"
-                //   />
-                //   <span className="cardsss--two__rect"></span>
-                //   <span className="cardsss--two__tri"></span>
-                //   <p>{element.name}</p>
-                //   <ul className="cardsss__list">
-                //     <li>
-                //       <a href={element.linked} target="_blank">
-                //       <i className="fab fa-linkedin-in"></i>
-                //       </a>
-                //     </li>
-                //   </ul>
-                // </div>
-            
-                // <div
-                //    key={element.id}
-                //    className=" col-sm-6 col-md-5 col-lg-3 my-2"
-                //  >
-                //    <div className="cardss">
-                //    <p className="memName">{element.name}</p>
-
-                //      <img
-                //        src={element.imgSrc}
-                //        alt={element.name}
-                //        className="memImg"
-                //      />
-                //    <ul>
-                //        <li>
-                //          <a href={element.linked} target="_blank">
-                //            <i className="fa-brands fa-linkedin"></i>
-                //          </a>
-                //        </li>
-                //      </ul>
-                //    </div>
-                //  </div>
-
-
-                // <div
-                //   key={element.id}
-                //   className="col-sm-6 col-md-5 col-lg-3 outest"
-                // >
-                //   <div className="outer">
-                //     <div className="pic">
-                //     <img
-                //       src={element.imgSrc}
-                //       alt={element.name}
-                //       className="img-round"
-                //     />
-                //     </div>
-                //     <div className="line m-4"></div>
-                //     <div className="Mname">
-                //       <p>{element.name}</p>
-                //     </div>
-                //     <div className="icons">
-                //     <a href={element.linked} target="_blank">
-                //             <i className="fa-brands fa-linkedin"></i>
-                //           </a>
-                //     </div>
-                    
-                    
-                    
-                //   </div>
-                // </div>
-
-
-                //suggestion try
-                <div
-                  key={element.id}
-                  className="col-sm-6 col-md-5 col-lg-3 outest"
-                >
-                <div className="cardss">
-                  
-                  <div className="img-cardss">
-                    
-                    <img
-                      src={element.imgSrc}
-                       alt={element.name}
-                       className="img-round"
-                 />
-                 <div className="gradient-overlay"></div>
-                 
-                  
-                  
-                 <div className="Mname">
-                       <p>{element.name}</p>
-                  </div>
-
-                    <div className="icons">
-                      <a href={element.linked} target="_blank">
-                             <i className=" fa fa-brands fa-linkedin"></i>
-                      </a>
-                    </div>
-                    
-
-                  </div>
+    <div className="webteam-container">
+     
+      
+      <div className="team-grid">
+        {teamMembers.map((member, index) => (
+          <div key={index} className="team-card">
+            <div className="card-inner">
+              <div className="card-front">
+                <img 
+                  src={member.image}
+                  alt={member.name}
+                  className="member-image"
+                />
+                <div className="image-overlay"></div>
+                <div className="cyber-border-effect"></div>
+              </div>
+              
+              <div className="member-info">
+                <h3 className="member-name">{member.name}</h3>
+                <div className="social-links">
+                  <a 
+                    href={member.social.linkedin}
+                    className="social-link linkedin"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="fab fa-linkedin-in"></i>
+                  </a>
+                  <a 
+                    href={member.social.github}
+                    className="social-link github"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="fab fa-github"></i>
+                  </a>
                 </div>
-                  
-                </div>
-
-              );
-            })}
+              </div>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
-      <div 
-        className="alt-bg" 
-        style={{'--xPos':`${position.x}px`,'--yPos':`${position.y}px`}}
-      />
-      <Sponsors />
-      <Footer />
-    </>
+    </div>
   );
-}
+};
 
 export default WebTeam;
-
-
-
-
-
-
-
-
-
-// import React, {useEffect} from 'react'
-// import Sponsors from "../Common/SponsorSlide/Sponsors";
-// import Footer from "../Common/Footer/Footer";
-// import Heading from "../Common/Headings/Heading";
-// import "./Webteam.css";
-// import WebteamData from "../../Data/Webteam";
-
-// function WebTeam() {
-
-//   useEffect(() => {
-  
-//     document.title="Website Team | FootPrints'23"
-
-//   }, [])
-
-//   return (
-//     <>
-//       {/* <div className="cover">
-//         <Heading id="heading_ourteam" title="Web Team" />
-//       </div> */}
-//       <div className="webteam_header_img" id="parent_div">
-//         <div className="text-on-image-webteam">
-//           <Heading id="heading_ourteam" title="WEB TEAM" />
-//         </div>
-//       </div>
-//       {/* <section id="achivements" className="acheivements section-bg container"> */}
-//       <div className="container">
-//         <div className="coordinatesDiv">
-//           <div className="memcardsss row my-5">
-//             {WebteamData.map((element) => {
-//               return (
-//                 <div
-//                   key={element.id}
-//                   className="memBlock col-sm-6 col-md-5 col-lg-3 profile"
-//                   data-aos="flip-left"
-//                   data-aos-delay="100"
-//                   data-aos-duration="1200"
-//                 >
-//                   <div className="img-box">
-//                     <img
-//                       src={element.imgSrc}
-//                       alt={element.name}
-//                       className="memImg"
-//                     />
-//                     <ul>
-//                       <li>
-//                         <a href={element.linked} target="_blank">
-//                           <i className="fa-brands fa-linkedin"></i>
-//                         </a>
-//                       </li>
-//                     </ul>
-//                   </div>
-//                   <p className="memName">{element.name}</p>
-//                 </div>
-//               );
-//             })}
-//           </div>
-//         </div>
-//       </div>
-//       {/* </section> */}
-//       <Sponsors />
-//       <Footer />
-//     </>
-//   );
-// }
-
-// export default WebTeam;
-
-

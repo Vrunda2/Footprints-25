787f68080e73cfa12971679e414c5dd91d894c7e
Vrunda2 <vrundaradadiya38@gmail.com>
2025-01-25 09:55:54 +0530

    team changes

--- src/Data/TeamFP.js
+++ src/Data/TeamFP.js
@@ -1,27 +1,102 @@
 //Coordinators
+import missing from "../assets/Team/Missing.jpg";
+import akshit from "../assets/Team/Core/Akshit.jpg";
+import pulin from "../assets/Team/Core/Pulin.jpg";
+import raj from "../assets/Team/Core/Raj.jpg";
+import shubham from "../assets/Team/Core/Shubham.jpg";
+
+import dhaval from "../assets/Team/Core/Dhaval.jpg";
+import joy from "../assets/Team/Core/Joy.jpg";
+import ayesha from "../assets/Team/Core/Ayesha.jpg";
+import digvijay from "../assets/Team/Core/Digvijay.jpg";
+import heer from "../assets/Team/Core/Heer.jpg";
+import krunal from "../assets/Team/Core/Krunal.jpg";
+import meghana from "../assets/Team/Core/Meghana.jpg";
+import meher from "../assets/Team/Core/Meher.jpg";
+import nachiket from "../assets/Team/Core/Nachiket.jpg";
+import satchit from "../assets/Team/Core/Satchit.jpg";
+import sumit from "../assets/Team/Core/Sumit.jpg";
+import vishwa from "../assets/Team/Core/Vishwa.jpg";
+import vrunda from "../assets/Team/Core/Vrunda.jpg";
+import yana from "../assets/Team/Core/Yana.jpg";
+
+import ayushi from "../assets/Team/OT/Aayushi.jpg";
+import advaita from "../assets/Team/OT/Advaita.jpg";
+import amrutesh from "../assets/Team/OT/Amrutesh.jpg";
+import ansh from "../assets/Team/OT/Ansh.jpg";
+import aryan from "../assets/Team/OT/Aryan.jpg";
+import atik from "../assets/Team/OT/Atik.jpg";
+import ayush from "../assets/Team/OT/Ayush.jpg";
+import dev from "../assets/Team/OT/Dev.jpg";
+import devesh from "../assets/Team/OT/Devesh.jpg";
+import DhruvG from "../assets/Team/OT/DhruvG.jpg";
+import DhruvS from "../assets/Team/OT/DhruvS.jpg";
+import DhruvT from "../assets/Team/OT/DhruvT.jpg";
+
+import DhruvVy from "../assets/Team/OT/DhruvVy.jpg";
+import Dhun from "../assets/Team/OT/Dhun.jpg";
+import Dirgh from "../assets/Team/OT/Dirgh.jpg";
+import Diya from "../assets/Team/OT/Diya.jpg";
+import Forum from "../assets/Team/OT/Forum.jpg";
+import HetS from "../assets/Team/OT/HetS.jpg";
+import Hetvi from "../assets/Team/OT/Hetvi.jpg";
+import Isha from "../assets/Team/OT/Isha.jpg";
+import Kavya from "../assets/Team/OT/Kavya.jpg";
+import Krisha from "../assets/Team/OT/Krisha.jpg";
+import Lavya from "../assets/Team/OT/Lavya.jpg";
+import Madhav from "../assets/Team/OT/Madhav.jpg";
+import Manthan from "../assets/Team/OT/Manthan.jpg";
+import Meerat from "../assets/Team/OT/Meerat.jpg";
+
+import Mittal from "../assets/Team/OT/Mittal.jpg";
+import MSahal from "../assets/Team/OT/MSahal.jpg";
+import Niramay from "../assets/Team/OT/Niramay.jpg";
+import Nishi from "../assets/Team/OT/Nishi.jpg";
+import Pearl from "../assets/Team/OT/Pearl.jpg";
+import Prachi from "../assets/Team/OT/Prachi.jpg";
+import Pratik from "../assets/Team/OT/Pratik.jpg";
+import Priyanshu from "../assets/Team/OT/Priyanshu.jpg";
+import Rishi from "../assets/Team/OT/Rishi.jpg";
+import Sakshi from "../assets/Team/OT/Sakshi.jpg";
+import Shiv from "../assets/Team/OT/Shiv.jpg";
+import Shubham from "../assets/Team/OT/Shubham.jpg";
+import Siddhi from "../assets/Team/OT/Siddhi.jpg";
+import Suhani from "../assets/Team/OT/Suhani.jpg";
+import Vanshika from "../assets/Team/OT/Vanshika.jpg";
+import Vedangi from "../assets/Team/OT/Vedangi.jpg";
+import Vinay from "../assets/Team/OT/Vinay.jpg";
+import Viraj from "../assets/Team/OT/Viraj.jpg";
+import Yash from "../assets/Team/OT/Yash.jpg";
+import Yashvi from "../assets/Team/OT/Yashvi.jpg";
+import Zaahid from "../assets/Team/OT/Zaahid.jpg";
+
+
+
+
+
 const coordinatorsData = [
   {
     id: 1,
-    imgSrc: `../src/assets/Team/Core/Akshit.jpg`,
+    imgSrc:akshit,
     name: "Akshit Patel",
     linked: `https://www.linkedin.com/in/akshit-patel-a54643177`,
   },
   {
     id: 2,
-    imgSrc: `../src/assets/Team/Core/Pulin.jpg`,
+    imgSrc: pulin,
     name: "Pulin Makwana",
     linked: `https://www.linkedin.com/in/pulin-makwana?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app`,
   },
   {
     id: 3,
-    imgSrc: `../src/assets/Team/Core/Raj.jpg`,
+    imgSrc: raj,
     name: "Raj Lad",
     linked: `https://www.linkedin.com/in/raj-lad-33066b237?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app`,
   },
   
   {
     id: 4,
-    imgSrc: `../src/assets/Team/Core/Shubham.jpg`,
+    imgSrc: shubham,
     name: "Shubham Prajapati",
     linked: ``,
   },
@@ -32,13 +107,13 @@ const coordinatorsData = [
 const headsData = [
   {
     id: 1,
-    imgSrc: `../src/assets/Team/Core/Dhaval.jpg`,
+    imgSrc:dhaval,
     name: "Dhaval Saxena",
     linked: `https://www.linkedin.com/in/dhaval-saxena-207187259?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app`,
   },
   {
     id: 2,
-    imgSrc: `../src/assets/Team/Core/Joy.jpg`,
+    imgSrc: joy,
     name: "Joy Patel",
     linked: `https://www.linkedin.com/in/joy-patel-b2320b1a4?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app`,
   },
@@ -49,85 +124,85 @@ const headsData = [
 const ccData = [
   {
     id: 1,
-    imgSrc: `../src/assets/Team/Core/Ayesha.jpg`,
+    imgSrc: ayesha,
     name: "Ayesha Patel",
     linked: `http://www.linkedin.com/in/ayeshapatel07`,
   },
   {
     id: 2,
-    imgSrc: `../src/assets/Team/Core/Digvijay.jpg`,
+    imgSrc: digvijay,
     name: "Digvijay Shekhawat",
     linked: `https://www.linkedin.com/in/digvijay-shekhawat-14965b1a5?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app`,
   },
   {
     id: 3,
-    imgSrc: `../src/assets/Team/Core/Heer.jpg`,
+    imgSrc: heer,
     name: "Heer Mistry",
     linked: `https://www.linkedin.com/in/heer-mistry-816b89292?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app`,
   },
   {
     id: 4,
-    imgSrc: `../src/assets/Team/Core/Krunal.jpg`,
+    imgSrc: krunal,
     name: "Krunal Vankar",
     linked: `https://www.linkedin.com/in/krunal-vankar-764198253?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app`,
   },
   {
     id: 5,
-    imgSrc: `../src/assets/Team/Core/Meghana.jpg`,
+    imgSrc: meghana,
     name: "Meghana Vasava",
     linked: `https://www.linkedin.com/in/meghana-vasava-17242a261/`,
   },
   {
     id: 6,
-    imgSrc: `../src/assets/Team/Core/Meher.jpg`,
+    imgSrc: meher,
     name: "Meher Thadani",
     linked: ``,
   },
   {
     id: 7,
-    imgSrc: `../src/assets/Team/Core/Nachiket.jpg`,
+    imgSrc: nachiket,
     name: "Nachiket Karambelkar",
     linked: `https://www.linkedin.com/in/nachiket-karambelkar-976566259?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app`,
   },
   {
     id: 8,
-    imgSrc: `../src/assets/Team/Missing.jpg`,
+    imgSrc: missing,
     name: "Pavan Khemani",
     linked: ``,
   },
   {
     id: 9,
-    imgSrc: `../src/assets/Team/Missing.jpg`,
+    imgSrc: missing,
     name: "Sanket Agrawal",
     linked: ``,
   },
   {
     id: 10,
-    imgSrc: `../src/assets/Team/Core/Satchit.jpg`,
+    imgSrc: satchit,
     name: "Satchit Bhatt",
     linked: `https://www.linkedin.com/in/satchit-bhatt-9933a1253?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app`,
   },
   {
     id: 11,
-    imgSrc: `../src/assets/Team/Core/Sumit.jpg`,
+    imgSrc: sumit,
     name: "Sumit Vankar",
     linked: `https://www.linkedin.com/in/sumit-vankar-429bbb284?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app`,
   },
   {
     id: 12,
-    imgSrc: `../src/assets/Team/Core/Vishwa.jpg`,
+    imgSrc: vishwa,
     name: "Vishwa Prajapati",
     linked: ``,
   },
   {
     id: 13,
-    imgSrc: `../src/assets/Team/Core/Vrunda.jpg`,
+    imgSrc: vrunda,
     name: "Vrunda Radadiya",
     linked: `https://www.linkedin.com/in/vrunda-radadiya-8a1a43257/`,
   },
   {
     id: 14,
-    imgSrc: `../src/assets/Team/Core/Yana.jpg`,
+    imgSrc: yana,
     name: "Yana Velani",
     linked: `https://www.linkedin.com/in/yana-velani-3002702a4?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app`,
   },
@@ -137,37 +212,37 @@ const ccData = [
 const orgcData = [
   {
     id: 1,
-    imgSrc: `../src/assets/Team/OT/Aayushi.jpg`,
+    imgSrc: ayushi,
     name: "Aayushi Pandey",
   },
   {
     id: 2,
-    imgSrc: `../src/assets/Team/OT/Advaita.jpg`,
+    imgSrc: advaita,
     name: "Advaita Tandel",
   },
   {
     id: 3,
-    imgSrc: `../src/assets/Team/OT/Amrutesh.jpg`,
+    imgSrc: amrutesh,
     name: "Amrutesh Sharma",
   },
   {
     id: 4,
-    imgSrc: `../src/assets/Team/OT/Ansh.jpg`,
+    imgSrc: ansh,
     name: "Ansh Dhake",
   },
   {
     id: 5,
-    imgSrc: `../src/assets/Team/OT/Aryan.jpg`,
+    imgSrc: aryan,
     name: "Aryan Dangariya",
   },
   {
     id: 6,
-    imgSrc: `../src/assets/Team/OT/Atik.jpg`,
+    imgSrc: atik,
     name: "Atik Chavda",
   },
   {
     id: 7,
-    imgSrc: `../src/assets/Team/OT/Ayush.jpg`,
+    imgSrc: ayush,
     name: "Ayush Alsika",
   },
   // {
@@ -177,27 +252,27 @@ const orgcData = [
   // },
   {
     id: 9,
-    imgSrc: `../src/assets/Team/OT/Dev.jpg`,
+    imgSrc: dev,
     name: "Dev Sanchla",
   },
   {
     id: 10,
-    imgSrc: `../src/assets/Team/OT/Devesh.jpg`,
+    imgSrc: devesh,
     name: "Devesh Agrawal",
   },
   {
     id: 11,
-    imgSrc: `../src/assets/Team/OT/DhruvG.jpg`,
+    imgSrc: DhruvG,
     name: "Dhruv Ghosiya",
   },
   {
     id: 11.5,
-    imgSrc: `../src/assets/Team/OT/DhruvS.jpg`,
+    imgSrc: DhruvS,
     name: "Dhruv Sharma",
   },
   {
     id: 12,
-    imgSrc: `../src/assets/Team/OT/DhruvT.jpg`,
+    imgSrc: DhruvT,
     name: "Dhruv Tapre",
   },
   // {
@@ -207,12 +282,12 @@ const orgcData = [
   // },
   {
     id: 14,
-    imgSrc: `../src/assets/Team/OT/DhruvVy.jpg`,
+    imgSrc: DhruvVy,
     name: "Dhruv Vyas",
   },
   {
     id: 15,
-    imgSrc: `../src/assets/Team/OT/Dhun.jpg`,
+    imgSrc: Dhun,
     name: "Dhun Sharma",
   },
   // {
@@ -222,42 +297,42 @@ const orgcData = [
   // },
   {
     id: 17,
-    imgSrc: `../src/assets/Team/OT/Dirgh.jpg`,
+    imgSrc: Dirgh,
     name: "Dirgh Jani",
   },
   {
     id: 18,
-    imgSrc: `../src/assets/Team/OT/Diya.jpg`,
+    imgSrc: Diya,
     name: "Diya Katara",
   },
   {
     id: 19,
-    imgSrc: `../src/assets/Team/OT/Forum.jpg`,
+    imgSrc: Forum,
     name: "Forum Vaghela",
   },
   {
     id: 20,
-    imgSrc: `../src/assets/Team/OT/HetS.jpg`,
+    imgSrc: HetS,
     name: "Het Shah",
   },
   {
     id: 21,
-    imgSrc: `../src/assets/Team/OT/Hetvi.jpg`,
+    imgSrc: Hetvi,
     name: "Hetvi Jajal",
   },
   {
     id: 22,
-    imgSrc: `../src/assets/Team/OT/Isha.jpg`,
+    imgSrc: Isha,
     name: "Isha Singh",
   },
   {
     id: 23,
-    imgSrc: `../src/assets/Team/OT/Kavya.jpg`,
+    imgSrc:Kavya,
     name: "Kavya Shirke",
   },
   {
     id: 24,
-    imgSrc: `../src/assets/Team/OT/Krisha.jpg`,
+    imgSrc: Krisha,
     name: "Krisha Gandhi",
   },
   // {
@@ -267,22 +342,22 @@ const orgcData = [
   // },
   {
     id: 26,
-    imgSrc: `../src/assets/Team/OT/Lavya.jpg`,
+    imgSrc: Lavya,
     name: "Lavya Padhiyar",
   },
   {
     id: 27,
-    imgSrc: `../src/assets/Team/OT/Madhav.jpg`,
+    imgSrc:Madhav,
     name: "Madhav Thakkar",
   },
   {
     id: 28,
-    imgSrc: `../src/assets/Team/OT/Manthan.jpg`,
+    imgSrc: Manthan,
     name: "Manthan Sathwara",
   },
   {
     id: 29,
-    imgSrc: `../src/assets/Team/OT/Meerat.jpg`,
+    imgSrc: Meerat,
     name: "Meerat Dave",
   },
   // {
@@ -292,127 +367,127 @@ const orgcData = [
   // },
   {
     id: 31,
-    imgSrc: `../src/assets/Team/OT/Mittal.jpg`,
+    imgSrc: Mittal,
     name: "Mittal Pawar",
   },
   {
     id: 32,
-    imgSrc: `../src/assets/Team/OT/MSahal.jpg`,
+    imgSrc: MSahal,
     name: "Mohammad Sahal Jethra",
   },
   {
     id: 33,
-    imgSrc: `../src/assets/Team/Missing.jpg`,
+    imgSrc:missing,
     name: "Mokshesh Shah",
   },
   {
     id: 34,
-    imgSrc: `../src/assets/Team/OT/Niramay.jpg`,
+    imgSrc: Niramay,
     name: "Niramay Patel",
   },
   {
     id: 35,
-    imgSrc: `../src/assets/Team/OT/Nishi.jpg`,
+    imgSrc: Nishi,
     name: "Nishi Modi",
   },
   {
     id: 36,
-    imgSrc: `../src/assets/Team/OT/Pearl.jpg`,
+    imgSrc: Pearl,
     name: "Pearl Sharma",
   },
   {
     id: 37,
-    imgSrc: `../src/assets/Team/OT/Prachi.jpg`,
+    imgSrc: Prachi,
     name: "Prachi Patel",
   },
   {
     id: 38,
-    imgSrc: `../src/assets/Team/OT/Pratik.jpg`,
+    imgSrc: Pratik,
     name: "Pratik Ramani",
   },
   {
     id: 39,
-    imgSrc: `../src/assets/Team/OT/Priyanshu.jpg`,
+    imgSrc: Priyanshu,
     name: "Priyanshu Chauhan",
   },
   {
     id: 40,
-    imgSrc: `../src/assets/Team/Missing.jpg`,
+    imgSrc: missing,
     name: "Rajal Mandalia",
   },
   {
     id: 41,
-    imgSrc: `../src/assets/Team/OT/Rishi.jpg`,
+    imgSrc: Rishi,
     name: "Rishi Shah",
   },
   {
     id: 42,
-    imgSrc: `../src/assets/Team/OT/Sakshi.jpg`,
+    imgSrc: Sakshi,
     name: "Sakshi Patel",
   },
   {
     id: 43,
-    imgSrc: `../src/assets/Team/OT/Shiv.jpg`,
+    imgSrc:Shiv,
     name: "Shiv Patel",
   },
   {
     id: 44,
-    imgSrc: `../src/assets/Team/OT/Shubham.jpg`,
+    imgSrc:Shubham,
     name: "Shubham Mahyavanshi",
   },
   {
     id: 45,
-    imgSrc: `../src/assets/Team/OT/Siddhi.jpg`,
+    imgSrc: Siddhi,
     name: "Siddhi Jani",
   },
   {
     id: 46,
-    imgSrc: `../src/assets/Team/OT/Suhani.jpg`,
+    imgSrc: Suhani,
     name: "Suhani Sharma",
   },
   {
     id: 47,
-    imgSrc: `../src/assets/Team/Missing.jpg`,
+    imgSrc: missing,
     name: "Sujal Kumar",
   },
   {
     id: 48,
-    imgSrc: `../src/assets/Team/OT/Vanshika.jpg`,
+    imgSrc: Vanshika,
     name: "Vanshika Mathur",
   },
   {
     id: 49,
-    imgSrc: `../src/assets/Team/OT/Vedangi.jpg`,
+    imgSrc: Vedangi,
     name: "Vedangi Prajapati",
   },
   {
     id: 50,
-    imgSrc: `../src/assets/Team/OT/Vinay.jpg`,
+    imgSrc: Vinay,
     name: "Vinay Thakur",
   },
   {
     id: 51,
-    imgSrc: `../src/assets/Team/OT/Viraj.jpg`,
+    imgSrc:Viraj,
     name: "Viraj Kapadia",
   },
   {
     id: 52,
-    imgSrc: `../src/assets/Team/Missing.jpg`,
+    imgSrc: missing,
     name: "Vraj Shah",
   },
   {
     id: 53,
-    imgSrc: `../src/assets/Team/OT/Yash.jpg`,
+    imgSrc: Yash,
     name: "Yash Parekh",
   },
   {
     id: 54,
-    imgSrc: `../src/assets/Team/OT/Yashvi.jpg`,
+    imgSrc: Yashvi,
     name: "Yashvi Shah",
   },
   {
     id: 55,
-    imgSrc: `../src/assets/Team/OT/Zaahid.jpg`,
+    imgSrc: Zaahid,
     name: "Zaahid Vohra",
   },
   // {

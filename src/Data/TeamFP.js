c5cb3844626903052009de6c1f27fa4348640ee5
Vrunda2 <vrundaradadiya38@gmail.com>
2025-02-12 14:17:00 +0530

    pulin photo

--- src/Data/TeamFP.js
+++ src/Data/TeamFP.js
@@ -1,7 +1,7 @@
 //Coordinators
 import missing from "../assets/Team/Missing.jpg";
 import akshit from "../assets/Team/Core/akshit1.jpg";
-import pulin from "../assets/Team/Core/pulin.jpg";
+import pulin from "../assets/Team/Core/Pulin.jpg";
 import raj from "../assets/Team/Core/Raj.jpg";
 import shubham from "../assets/Team/Core/shubham1.jpg";
 
